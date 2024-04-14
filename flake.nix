cdd67f4779765c91cade7e3cdb785dfcf0b9ff71
Green Sky <green@g-s.xyz>
2024-04-14 10:57:07 +0200

    fix flake and pull in os zstd fixes

--- flake.nix
+++ flake.nix
@@ -58,6 +58,9 @@
         cmakeFlags = [
           "-DTOMATO_ASAN=OFF"
           "-DCMAKE_BUILD_TYPE=RelWithDebInfo"
+          "-DFETCHCONTENT_SOURCE_DIR_JSON=${pkgs.nlohmann_json.src}" # we care less about version here
+          # do we really care less about the version? do we need a stable abi?
+          "-DFETCHCONTENT_SOURCE_DIR_ZSTD=${pkgs.zstd.src}" # TODO: use package instead
         ];
 
         # TODO: replace with install command
