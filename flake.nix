11d8e976ca25fba1946c106119f8caefef4eff54
Green Sky <Green-Sky@users.noreply.github.com>
2025-03-11 11:57:37 +0100

    update sdl to 3.2.8

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       flake = false;
     };
     sdl3 = {
-      url = "github:libsdl-org/SDL/b5c3eab6b447111d3c7879bb547b80fb4abd9063"; # keep in sync with cmake
+      url = "github:libsdl-org/SDL/f6864924f76e1a0b4abaefc76ae2ed22b1a8916e"; # keep in sync with cmake
       flake = false;
     };
     sdl3_image = {
