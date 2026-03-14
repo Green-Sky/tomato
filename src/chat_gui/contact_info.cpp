8de646200be8fe778d6568c560b3cec9e2bc3864
Green Sky <Green-Sky@users.noreply.github.com>
2026-03-14 09:21:02 +0100

    contact info table

--- src/chat_gui/contact_info.cpp
+++ src/chat_gui/contact_info.cpp
@@ -14,22 +14,38 @@ void renderContactInfo(
 
 	std::string last_cat;
 
+	if (!ImGui::BeginTable("contactinfo", 2, ImGuiTableFlags_SizingFixedFit | ImGuiTableFlags_BordersInnerV)) {
+		return;
+	}
+	ImGui::TableSetupColumn("name");
+	ImGui::TableSetupColumn("string");
+
 	for (const auto& it : c2s_list) {
 		if (it.advanced != advanced) {
 			continue;
 		}
 		if (it.category != last_cat) {
+			ImGui::EndTable();
 			last_cat = it.category;
 			ImGui::SeparatorText(last_cat.c_str());
+			if (!ImGui::BeginTable("contactinfo", 2, ImGuiTableFlags_SizingFixedFit | ImGuiTableFlags_BordersInnerV)) {
+				return; // ???
+			}
+			ImGui::TableSetupColumn("name");
+			ImGui::TableSetupColumn("string");
 		}
 
+		ImGui::TableNextColumn();
 		ImGui::TextUnformatted(it.name.data(), it.name.data()+it.name.size());
 
-		const std::string str = it.fn(c, verbose);
-		if (!str.empty()) {
-			ImGui::SameLine();
-			ImGui::TextUnformatted(str.data(), str.data()+str.size());
+		if (ImGui::TableNextColumn()) {
+			const std::string str = it.fn(c, verbose);
+			if (!str.empty()) {
+				//ImGui::SameLine();
+				ImGui::TextUnformatted(str.data(), str.data()+str.size());
+			}
 		}
 	}
+	ImGui::EndTable();
 }
 
